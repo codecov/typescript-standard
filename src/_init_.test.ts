3e94c9036e7e0c49c6a88d4300421c3e04d68938
Terry Smith <terry@designcollider.com>
2020-05-08 16:11:42 -0300

    test data

--- src/_init_.test.ts
+++ src/_init_.test.ts
@@ -1,11 +1,6 @@
 import Init from "./_init_";
 
 describe('init', () => {
-    test('test uncovered if', () => {
-        const indexObj = new Init();
-        expect(indexObj.uncovered_if()).toEqual(false);
-    });
-
     test('fully covered', () => {
         const indexObj = new Init();
         expect(indexObj.fully_covered()).toEqual(true);
