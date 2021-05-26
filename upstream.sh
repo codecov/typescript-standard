17493c352e121d10c3e64eb3335e2681501449f7
Adrian Viquez <aviquez96@gmail.com>
2021-05-25 19:52:37 -0700

    Change upstream's path to coverage source file

--- upstream.sh
+++ upstream.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this to the coverage file you want to upload to the standards repo
-COVERAGE_SOURCE_FILE=/home/travis/build/codecov/typescript-standard/coverage/coverage-final.json
+COVERAGE_SOURCE_FILE=./coverage/coverage-final.json
 
 # -=- Nothing below here should need to be changed -=-
 
