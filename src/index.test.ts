912a7e6962bbe52ee773be4090cfdb2103a55623
nicholas-codecov <nicholas.deschenes@codecov.io>
2022-10-04 15:22:12 -0300

    update tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,21 @@
 import Index from "./index";
 
-test('test uncovered if', () => {
+test('test uncovered if true', () => {
     const indexObj = new Index();
     expect(indexObj.uncovered_if()).toEqual(false);
+});
+
+test('test uncovered if false', () => {
+    const indexObj = new Index();
+    expect(indexObj.uncovered_if(false)).toEqual(true);
 });
 
 test('fully covered', () => {
     const indexObj = new Index();
     expect(indexObj.fully_covered()).toEqual(true);
 });
+
+test('all covered', () => {
+    const indexObj = new Index()
+    expect(indexObj.uncovered()).toEqual(true)
+})
